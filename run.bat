25067647dbc710883badb233b5a6cca8d4309f4e
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-06-30 11:28:09 +0600

    fixed pwsh script leves empty folders

--- run.bat
+++ run.bat
@@ -1,2 +1,5 @@
 powershell.exe -ExecutionPolicy Bypass -File "Build.ps1" -arch "32" -namePattern "winlibs-i686-posix-dwarf-gcc-[0-9.]+-mingw-w64ucrt-(.*?).7z$"
 powershell.exe -ExecutionPolicy Bypass -File "Build.ps1" -arch "64" -namePattern "winlibs-x86_64-posix-seh-gcc-[0-9.]+-mingw-w64ucrt-(.*?).7z$"
+
+IF EXIST "%~dp0mingw64" (RMDIR "%~dp0mingw64")
+IF EXIST "%~dp0mingw32" (RMDIR "%~dp0mingw32")
