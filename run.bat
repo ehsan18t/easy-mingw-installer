a07e4708ee12be8d523ff75e3d59414dfd8bdc25
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-07-01 21:59:26 +0600

    wait to close manually after build complete

--- run.bat
+++ run.bat
@@ -1,5 +1,9 @@
-powershell.exe -ExecutionPolicy Bypass -File "Build.ps1" -arch "32" -namePattern "winlibs-i686-posix-dwarf-gcc-[0-9.]+-mingw-w64ucrt-(.*?).7z$"
+@ECHO OFF
+
+@REM powershell.exe -ExecutionPolicy Bypass -File "Build.ps1" -arch "32" -namePattern "winlibs-i686-posix-dwarf-gcc-[0-9.]+-mingw-w64ucrt-(.*?).7z$"
 powershell.exe -ExecutionPolicy Bypass -File "Build.ps1" -arch "64" -namePattern "winlibs-x86_64-posix-seh-gcc-[0-9.]+-mingw-w64ucrt-(.*?).7z$"
 
 IF EXIST "%~dp0mingw64" (RMDIR "%~dp0mingw64")
 IF EXIST "%~dp0mingw32" (RMDIR "%~dp0mingw32")
+
+PAUSE
