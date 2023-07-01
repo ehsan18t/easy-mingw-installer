dc64b331ecec43191b0eb878b2007f75c4594fc0
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-07-01 22:16:29 +0600

    cleanup before start build

--- run.bat
+++ run.bat
@@ -1,9 +1,17 @@
 @ECHO OFF
 
+IF EXIST "%~dp0Output" (RMDIR /S /Q "%~dp0Output")
+CALL :CleanUp
+
 powershell.exe -ExecutionPolicy Bypass -File "Build.ps1" -arch "32" -namePattern "winlibs-i686-posix-dwarf-gcc-[0-9.]+-mingw-w64ucrt-(.*?).7z$"
 powershell.exe -ExecutionPolicy Bypass -File "Build.ps1" -arch "64" -namePattern "winlibs-x86_64-posix-seh-gcc-[0-9.]+-mingw-w64ucrt-(.*?).7z$"
 
-IF EXIST "%~dp0mingw64" (RMDIR "%~dp0mingw64")
-IF EXIST "%~dp0mingw32" (RMDIR "%~dp0mingw32")
-
+CALL :CleanUp
 PAUSE
+EXIT
+
+:CleanUp
+IF EXIST "%~dp0*.7z" (DEL "%~dp0*.7z")
+IF EXIST "%~dp0mingw64" (RMDIR /S /Q "%~dp0mingw64")
+IF EXIST "%~dp0mingw32" (RMDIR /S /Q "%~dp0mingw32")
+EXIT /B
